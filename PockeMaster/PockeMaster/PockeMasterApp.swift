7cc8e88508762c362e255734062743f54f1fd422
CWH <>
2023-05-04 09:32:36 +0800

    PockerMaster 更新

---
+++ PockeMaster/PockeMaster/PockeMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PockeMasterApp.swift
+//  PockeMaster
+//
+//  Created by CWH on 2023/5/3.
+//
+
+import SwiftUI
+
+@main
+struct PockeMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

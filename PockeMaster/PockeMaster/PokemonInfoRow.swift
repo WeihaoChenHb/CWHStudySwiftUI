1459c7b875ab3e90c6e1245cdaf9ede8daf8f8f0
CWH <>
2023-05-06 16:06:55 +0800

    pocker

---
+++ PockeMaster/PockeMaster/PokemonInfoRow.swift
@@ -0,0 +1,12 @@
+//
+//  PokemonInfoRow.swift
+//  PockeMaster
+//
+//  Created by CWH on 2023/5/4.
+//
+
+import SwiftUI
+
+struct PokemonInfoRow: View {
+    let mode = PokemonViewModel.sample(id:1)
+}
